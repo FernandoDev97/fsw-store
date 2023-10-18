8e71a1d340112659e0227f46837a15db6f1f3434
Fernando Souza <l.fernando.dev@gmail.com>
2023-10-17 23:30:05 -0300

    feat: finished styles home page mobile version

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image"
 import { Categories } from "./components/categories"
 import { ProductList } from "./components/product-list"
 import { prismaClient } from "@/lib/prisma"
+import { SectionTitle } from "./components/section-title"
+import { PromoBanner } from "./components/promo-banner"
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,14 +14,26 @@ export default async function Home() {
     }
   })
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      }
+    }
+  })
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      }
+    }
+  })
+
   return (
     <div>
-      <Image
-        className="h-auto w-full px-5"
+      <PromoBanner
         src="/Banner.png"
-        width={0}
-        height={0}
-        sizes="100vw"
         alt="Até 55% de desconto este mês"
       />
 
@@ -28,18 +42,35 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
-        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
-        <ProductList products={deals}/>
+        <SectionTitle>
+          Ofertas
+        </SectionTitle>
+        <ProductList products={deals} />
       </div>
 
-      <Image
-        className="h-auto w-full px-5"
-        src="/banner-mouses.png"
-        width={0}
-        height={0}
-        sizes="100vw"
-        alt="Até 5% de desconto em mouses"
+      <PromoBanner
+        src='/banner-mouses.png'
+        alt="Até 55% de desconto em mouses"
       />
+
+      <div className="mt-8">
+        <SectionTitle>
+          Mouses
+        </SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
+      <PromoBanner
+        src='/banner-fones.png'
+        alt="Até 20% de desconto em Fones"
+      />
+
+      <div className="mt-8">
+        <SectionTitle>
+          Fones
+        </SectionTitle>
+        <ProductList products={mouses} />
+      </div>
     </div>
   )
 }
