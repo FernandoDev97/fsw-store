40a8369abad5d5865b25efff25a97c01085e6dff
Fernando Souza <l.fernando.dev@gmail.com>
2023-10-17 22:52:21 -0300

    feat: add price to list products components

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,14 +1,16 @@
-import { Product } from "@prisma/client"
+import { ProductWithTotalPrice } from "@/helpers/product"
 import Image from "next/image"
+import { Badge } from "./badge"
+import { ArrowDownIcon } from "lucide-react"
 
 interface ProductItemProps {
-    product: Product
+    product: ProductWithTotalPrice
 }
 
 export const ProductItem = ({ product }: ProductItemProps) => {
     return (
         <div className="flex flex-col max-w-[156px] gap gap-4">
-            <div className="bg-accent rounded-lg h-[170px] w-[156px] flex justify-center items-center">
+            <div className="bg-accent relative rounded-lg h-[170px] w-[156px] flex justify-center items-center">
                 <Image
                     src={product.imageUrls[0]}
                     width={0}
@@ -17,11 +19,27 @@ export const ProductItem = ({ product }: ProductItemProps) => {
                     className="max-h-[70%] w-auto object-contain max-w-[80%] h-auto"
                     alt={product.name}
                 />
+                {product.discountPercentage > 0 && (
+                    <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+                        <ArrowDownIcon size={14} /> {product.discountPercentage}%
+                    </Badge>
+                )}
             </div>
-            <div>
+
+            <div className="flex flex-col gap-1">
                 <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
                     {product.name}
                 </p>
+                <div className="flex items-center gap-2">
+                    {product.discountPercentage > 0 ? (
+                        <>
+                            <p className="font-semibold text-base">R$ {product.totalPrice.toFixed(2)}</p>
+                            <p className="opacity-75 text-xs line-through">R$ {Number(product.basePrice.toFixed(2))}</p>
+                        </>
+                    ) : (
+                        <p className="opacity-75 line-through">R$ {Number(product.basePrice.toFixed(2))}</p>
+                    )}
+                </div>
             </div>
         </div>
     )
--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,21 +1,24 @@
 import { Product } from "@prisma/client";
 
-interface ProductWithTotalPrice extends Product {
-    totalPrice: number
+export interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
 }
 
-const computeProductsTotalPrice = (product: Product): ProductWithTotalPrice => {
-    if(product.discountPercentage === 0) {
-        return {
-            ...product,
-            totalPrice: Number(product.basePrice),
-        }
-    }
-
-    const totalPrice = Number(product.basePrice) * Number(product.discountPercentage / 100)
-
+export const computeProductsTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
     return {
-        ...product,
-        totalPrice,
-    }
-}
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const totalPrice =
+    Number(product.basePrice) * Number(product.discountPercentage / 100);
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
