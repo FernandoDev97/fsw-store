1d33122d193ca7c55fd983de01d60c1135e1b44f
Fernando Souza <l.fernando.dev@gmail.com>
2023-10-17 21:05:22 -0300

    feat: add Categories and CategoryItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,23 @@
 'use client'
 
 import Image from "next/image"
+import { Categories } from "./components/categories"
 
 export default function Home() {
   return (
     <div className="p-5">
       <Image
-        className="h-auto w-full" 
+        className="h-auto w-full"
         src="/Banner.png"
         width={0}
         height={0}
         sizes="100vw"
         alt="Até 5% de desconto este mês"
       />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   )
 }
